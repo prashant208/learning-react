4fb33d52d874b296d0783705db34048bd2bcc2ab
prashant208 <vb05446@gmail.com>
2024-03-03 20:11:34 +0530

    npx parcel build index.html

--- Day 2/App.js
+++ Day 2/App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", {id : "Parent"}, 
 React.createElement("div", {id : "Child"},
-[React.createElement("h1", {}, "Hello H1"), React.createElement("h2", {}, "Hello H2")]
+[React.createElement("h1", {}, "Hello H1"), React.createElement("h2", {}, "Hello H2"), React.createElement("h3", {}, "Hello H3")]
 ));
 
 
