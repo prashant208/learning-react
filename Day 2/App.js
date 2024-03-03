0c136f579d5ae202dd1f72905050b535d9766a57
prashant208 <vb05446@gmail.com>
2024-03-03 12:10:51 +0530

    Day 2

---
+++ Day 2/App.js
@@ -0,0 +1,10 @@
+const parent = React.createElement("div", {id : "Parent"}, 
+React.createElement("div", {id : "Child"},
+[React.createElement("h1", {}, "Hello H1"), React.createElement("h2", {}, "Hello H2")]
+));
+
+
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
